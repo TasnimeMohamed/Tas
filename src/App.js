2773eb971c42c6bd0532612d4458c7bd2ae24594
Tasnimeee <tasneem.mohamed@elmenus.com>
2019-04-03 17:48:20 +0200

    new task

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import 'semantic-ui-css/semantic.min.css'
+import { Table } from 'semantic-ui-react'
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <div>
+          <Table fixed >
+    <Table.Header>
+      <Table.Row>
+        <Table.HeaderCell>ID</Table.HeaderCell>
+        <Table.HeaderCell>Item</Table.HeaderCell>        
+        <Table.HeaderCell>Price</Table.HeaderCell>
+        <Table.HeaderCell>Description</Table.HeaderCell>
+      </Table.Row>
+    </Table.Header>
+
+    <Table.Body>
+      <Table.Row>
+        <Table.Cell>1</Table.Cell>
+        <Table.Cell>Pizza</Table.Cell>
+        <Table.Cell>22LE</Table.Cell>
+        <Table.Cell>
+          John is an interesting boy but sometimes you don't really have enough room to describe
+          everything you'd like
+        </Table.Cell>
+      </Table.Row>
+      
+      
+    </Table.Body>
+  </Table>
+
+            </div>
         </header>
       </div>
     );
