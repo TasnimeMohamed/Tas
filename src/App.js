1a6e827c9ee1ba5ce0d864288ebb3ad096a14eae
Tasnimeee <tasneem.mohamed@elmenus.com>
2019-04-04 11:25:43 +0200

    task

--- src/App.js
+++ src/App.js
@@ -1,34 +1,54 @@
 import React, { Component } from 'react';
 import './App.css';
 import 'semantic-ui-css/semantic.min.css'
-import { Table } from 'semantic-ui-react'
+import { Table, Button, Form, Input } from 'semantic-ui-react'
 
 
 class App extends Component {
+  state = {}
+
+  handleChange = (e, { value }) => this.setState({ value })
+
+  
   render() {
+    const { value } = this.state
+    
     return (
+   
       <div className="App">
         <header className="App-header">
-          <div>
-          <Table fixed >
+        <div>
+        <Form>
+      <Form.Group widths='equal'>
+        <Form.Field control={Input}  placeholder='Item' />
+        <Form.Field control={Input}  placeholder='Description' />
+        <Form.Field control={Input}  placeholder='Price' />
+
+     <Form.Field  className= "Button"control={Button}>Submit</Form.Field>
+        </Form.Group>
+    </Form>
+          </div>
+          <div >
+            
+          <Table fixed className="table">
     <Table.Header>
       <Table.Row>
-        <Table.HeaderCell>ID</Table.HeaderCell>
         <Table.HeaderCell>Item</Table.HeaderCell>        
-        <Table.HeaderCell>Price</Table.HeaderCell>
         <Table.HeaderCell>Description</Table.HeaderCell>
+        <Table.HeaderCell>Price</Table.HeaderCell>
+
       </Table.Row>
     </Table.Header>
 
     <Table.Body>
       <Table.Row>
-        <Table.Cell>1</Table.Cell>
         <Table.Cell>Pizza</Table.Cell>
-        <Table.Cell>22LE</Table.Cell>
         <Table.Cell>
           John is an interesting boy but sometimes you don't really have enough room to describe
           everything you'd like
         </Table.Cell>
+        <Table.Cell>22LE</Table.Cell>
+
       </Table.Row>
       
       
